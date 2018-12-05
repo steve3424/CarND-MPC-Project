3f75240b00feb6e19c71499a9af83e3218c8d584
Steve <stevefrazee123@gmail.com>
2018-12-04 20:18:14 -0500

    add vehicle model constraints

--- src/MPC.cpp
+++ src/MPC.cpp
@@ -23,7 +23,7 @@ double dt = 0;
 const double Lf = 2.67;
 
 // reference velocity to avoid stopping
-ref_v = 40;
+double ref_v = 40;
 
 // Save indices where each variable starts
 size_t x_start = 0;
@@ -50,17 +50,62 @@ class FG_eval {
     
     // COST FUNCTION 
     // add cte, epsi, and reference velocity (to prevent stopping) 
-    for (int i = 0; i < N; i++) {
-   	fg[0] += CppAD::pow(vars[cte_start + i], 2);
-        fg[0] += CppAD::pow(vars[epsi_start + i], 2);
-	fg[0] += CppAD::pow(vars[v_start + i] - ref_v, 2);	
+    for (int t = 0; t < N; t++) {
+   	fg[0] += CppAD::pow(vars[cte_start + t], 2);
+        fg[0] += CppAD::pow(vars[epsi_start + t], 2);
+	fg[0] += CppAD::pow(vars[v_start + t] - ref_v, 2);	
     }
 
     // minimize actuator use 
-    for (int i = 0; i < N-1; i++) {
-    	fg[0] += CppAD::pow(vars[delta_start + i], 2);
-	fg[0] += CppAD::pow(vars[a_start + i], 2);
+    for (int t = 0; t < N-1; t++) {
+    	fg[0] += CppAD::pow(vars[delta_start + t], 2);
+	fg[0] += CppAD::pow(vars[a_start + t], 2);
     }
+
+
+    // VEHICLE MODEL - set up constraints for vehicle model
+    // set initial constraints to initial state
+    fg[1 + x_start] = vars[x_start];
+    fg[1 + y_start] = vars[y_start];
+    fg[1 + psi_start] = vars[psi_start];
+    fg[1 + v_start] = vars[v_start];
+    fg[1 + cte_start] = vars[cte_start];
+    fg[1 + epsi_start] = vars[epsi_start];
+
+    for (int t = 1; t < N; t++) {
+    	// state at t
+	AD<double> x0 = vars[x_start + t - 1];
+	AD<double> y0 = vars[y_start + t - 1];
+	AD<double> psi0 = vars[psi_start + t - 1];
+	AD<double> v0 = vars[v_start + t - 1];
+	AD<double> cte0 = vars[cte_start + t - 1];
+	AD<double> epsi0 = vars[epsi_start + t - 1];
+
+	// state at t+1
+	AD<double> x1 = vars[x_start + t];
+	AD<double> y1 = vars[y_start + t];
+	AD<double> psi1 = vars[psi_start + t];
+	AD<double> v1 = vars[v_start + t];
+	AD<double> cte1 = vars[cte_start + t];
+	AD<double> epsi1 = vars[epsi_start + t];
+
+	// actuation at time t
+	AD<double> delta0 = vars[delta_start + t - 1];
+	AD<double> a0 = vars[a_start + t - 1];
+
+	// errors at time t
+	AD<double> f0 = coeffs[0] + coeffs[1]*x0 + coeffs[2]*x0*x0 + coeffs[3]*x0*x0*x0;
+	AD<double> psides0 = CppAD::atan(coeffs[1] + 2*coeffs[2]*x0 + 3*coeffs[3]*x0*x0);
+
+	// kinematic model constraints
+	fg[1 + x_start + t] = x1 - (x0 + v0 * CppAD::cos(psi0) * dt);
+	fg[1 + y_start + t] = y1 - (y0 + v0 * CppAD::sin(psi0) * dt);
+	fg[1 + psi_start + t] = psi1 - (psi0 + v0 * delta0 / Lf * dt);
+        fg[1 + v_start + t] = v1 - (v0 + a0 * dt);
+	fg[1 + cte_start + t] = cte1 - ((y0 - f0) + (v0 * CppAD::sin(epsi0) * dt));	
+	fg[1 + epsi_start + t] = epsi1 - ((psi0 - psides0) + v0 * delta0 / Lf * dt);
+    }
+    
   }
 };
 
